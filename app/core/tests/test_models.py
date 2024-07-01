950a7fd944970bc57659a638bd309b4c63d4bbd9
yurii <yurii@ubuntu-vm.myguest.virtualbox.org>
2024-07-02 00:31:40 +0300

    Lesson 52 complated and linting fixed.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,13 +13,13 @@ class ModelTests(TestCase):
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password,
+            email=email,
+            password=password,
         )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-    
+
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
