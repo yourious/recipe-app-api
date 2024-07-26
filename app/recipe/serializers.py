905178e87373676070fa403987c6f5225be7ec15
yurii <element010796@gmail.com>
2024-07-26 10:52:51 +0300

    Lesson 88 complated.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -13,3 +13,10 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for the recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
