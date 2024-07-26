b7cdfdec6edc34cec5581a5d2d1874a6b1c9ddd9
yurii <element010796@gmail.com>
2024-07-26 17:13:56 +0300

    Lesson 94 complated.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,7 +3,10 @@ Serializers for recipe APIs
 """
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -12,7 +15,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
-        read_only = ['id']
+        read_only_fields = ['id']
 
 
 class RecipeDetailSerializer(RecipeSerializer):
@@ -20,3 +23,12 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
